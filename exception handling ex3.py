4d9adcbb6b8951c7768eaa66739828852904a72d
CHETHAN BADIGER <chetandvg2004@gmail.com>
2026-04-15 20:11:52 +0530

    Add files via upload

---
+++ exception handling ex3.py
@@ -0,0 +1,19 @@
+def fun1():
+    print("entering fun1")
+    try:
+        fun2()
+    except Exception as e:
+        print("error occured in fun1")
+    print("living fun1")
+def fun2():
+    print("entering fun2")
+    try:
+        res = 10/0
+        print(res)
+    except Exception as e:
+        print("error occured in fun2")
+        # raise e
+    print("living fun2")
+print("program start")
+fun1()
+print("program end")
