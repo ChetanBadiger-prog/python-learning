df560d9cc4d895e7714d72d36a71ce09dcbf0576
Chethan Badiger <chetandvg2004@gmail.com>
2026-05-06 21:09:22 +0530

    tuples

---
+++ list comprehension.py
@@ -0,0 +1,9 @@
+l = [1,2,3,4,5,6]
+l1 = [i for i in l if i%2==0]
+print(l1)
+
+
+l1 = list(range(1,11))
+print(l1)
+
+
---
+++ tuple 01.py
@@ -0,0 +1,8 @@
+t1 = (10)
+print( type(t1) )
+
+t2 = (10,)
+print( type( t2 ) )
+
+t3 = (10,20,30,40)
+print( type( t3 ) )
